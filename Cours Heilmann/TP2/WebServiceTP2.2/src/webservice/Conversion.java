e7e63ffd6f217e9ab9f9a9eb56a8a51f9a6ad460
HeilmannBrice <71489884+HeilmannBrice@users.noreply.github.com>
2020-10-22 00:04:37 +0200

    TP2

---
+++ Cours Heilmann/TP2/WebServiceTP2.2/src/webservice/Conversion.java
@@ -0,0 +1,14 @@
+package webservice;
+
+import javax.jws.WebMethod;
+import javax.jws.WebParam;
+import javax.jws.WebService;
+
+@WebService(targetNamespace = "http://www.polytech.fr")
+public class Conversion {
+
+	@WebMethod(operationName="convertir")
+	public double conversion(@WebParam(name="mt") double montant) {
+		return montant*1.6;
+	}
+}
